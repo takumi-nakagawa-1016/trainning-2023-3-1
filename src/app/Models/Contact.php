ebcb268d7c38bc31de0198a69356e7157381ff5a
takumi-nakagawa-1016 <takumi.nakagawa@arsaga.jp>
2023-03-08 14:14:33 +0900

    テスト

--- src/app/Models/Contact.php
+++ src/app/Models/Contact.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * Contactモデル　お問い合わせに関するモデル
+ * Contactモデル　お問い合わせのモデル
  * @param string $name 名前
  * @param string $email メールアドレス
  * @param string $content  お問い合わせ内容
